25306f2f08d87de9800b72e2722a61d56a119c6d
SND\kerryjiang_cp <SND\kerryjiang_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2011-07-14 15:18:35 +0000

    mainline: improved IronSocketServer quickstart project
    git-svn-id: https://SuperSocket.svn.codeplex.com/svn@68737 81fbe566-5dc4-48c1-bdea-7421811ca204

--- mainline/QuickStart/IronSocketServer/DynamicAppServer.cs
+++ mainline/QuickStart/IronSocketServer/DynamicAppServer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using SuperSocket.SocketBase;
 
-namespace SuperSocket.QuickStart.IronPythonServer
+namespace SuperSocket.QuickStart.IronSocketServer
 {
     public class DynamicAppServer : AppServer
     {
